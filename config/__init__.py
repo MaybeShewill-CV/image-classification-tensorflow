70f2219ff152f4ede56d071e310188b65427de83
luoyao <luoyao@baidu.com>
2021-03-24 17:26:12 +0800

    update config file

--- config/__init__.py
+++ config/__init__.py
@@ -1,7 +1,10 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 # @Time    : 2019/12/10 下午1:57
-# @Author  : LuoYao
-# @Site    : ICode
+# @Author  : MaybeShewill-CV
+# @Site    : https://github.com/MaybeShewill-CV/image-classification-tensorflow
 # @File    : __init__.py.py
 # @IDE: PyCharm
+"""
+config file
+"""
