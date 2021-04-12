97a580e3aa8679ddc2378d3fff461f3f695584ad
luoyao <luoyao@baidu.com>
2021-04-12 20:50:14 +0800

    update

--- cls_model_zoo/verify_repvgg_conv.py
+++ cls_model_zoo/verify_repvgg_conv.py
@@ -1,8 +1,8 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 # @Time    : 2021/4/9 下午2:31
-# @Author  : LuoYao
-# @Site    : ICode
+# @Author  : MaybeShewill-CV
+# @Site    : https://github.com/MaybeShewill-CV/image-classification-tensorflow
 # @File    : verify_repvgg_conv.py
 # @IDE: PyCharm
 """
