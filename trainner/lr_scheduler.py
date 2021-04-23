9effef673a37636bd4ce7330a26c34041e0724a7
MaybeShewill <howard327@163.com>
2021-04-24 02:17:37 +0800

    update

---
+++ trainner/lr_scheduler.py
@@ -0,0 +1,7 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+# @Time    : 2021/4/24 0:40
+# @Author  : MaybeShewill-CV
+# @Site    : https://github.com/MaybeShewill-CV
+# @File    : lr_scheduler.py
+# @IDE:    PyCharm
